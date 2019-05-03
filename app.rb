687fa474190aab8533f8126a33938ab89942727a
Adam S <adam.sarli@gmail.com>
2019-05-03 15:41:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,6 +31,7 @@ class App < Sinatra::Base
     @num1 = params[:number1].to_i 
     @num2 = params[:number2].to_i 
     @operation = params[:operation]
+    @num1.send(@operation, @num2).to_s
   end 
   
 end
